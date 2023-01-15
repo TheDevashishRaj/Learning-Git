a656536ed5a8c932b3f50463ecf7650f5cbb2f67
TheDevashishRaj <113200191+TheDevashishRaj@users.noreply.github.com>
2023-01-15 14:11:23 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // This is the preprocessor directive
 
-int main()
+int main() // The Main Function of the program. Program execution begins from here
 {
-    printf("Hello World\n");
-    return 0;
+    printf("Hello World\n"); // printf function is used to print a string onto the terminal window (screen).
+    return 0; // return statement returns a code, 0 if successful, otherwise unsuccessful.
 }
